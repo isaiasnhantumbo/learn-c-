1f9d0a54b05e89506ac887ba6dde316ac3e63a45
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2022-03-08 15:02:51 +0200

    Added basic of OOP exercises

---
+++ IntroducaoPOO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public String ToStringOverrided()
+    {
+       return $"Chamo-me {Name}, tenho {Age} anos de idade";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person("Isaias", 18);
+        Console.WriteLine(person1.ToStringOverrided());
+    }
+
+}
